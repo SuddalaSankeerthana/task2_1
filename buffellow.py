970e96e583568e9d8c569d70ef21f3d0471b525d
sankeerthanasuddala <127500161+Sankeerthanasuddala@users.noreply.github.com>
2023-03-27 20:10:26 +0530

    Bufellow is added

---
+++ buffellow.py
@@ -0,0 +1,9 @@
+class Buffellow:
+    def eat(self):
+        print("I can Eat")
+    def sleep(self):
+        print("I can sleep")
+    def run(self):
+        print("I can Run")
+    def sound(self):
+        print("I can Sound")
